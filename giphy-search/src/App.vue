edeb92610edc4385e94f13f8e93497a6ee1c9a7b
Javier Martínez Solís <reynaldo.martinez@coppel.com>
2019-05-15 11:33:40 -0600

    Giphy Search finalized

--- giphy-search/src/App.vue
+++ giphy-search/src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="vueLogo" alt="Vue logo" src="./assets/logo.png">
+    <GiphyForm
+      @searchGiphy="searchGiphy"
+    />
+
+    <ul>
+      <li 
+        v-for="image in images"
+        v-bind:key="image.id" 
+      >
+        <a
+          v-bind:href="image.url"
+          target="_blank"
+        >
+          <img
+            v-bind:src="image.images.preview_gif.url"
+          />
+        </a>
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GiphyForm from './components/GiphyForm.vue'
+import Axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GiphyForm
+  },
+  data() {
+    return {
+      images: []
+    }
+  },
+  methods: {
+    searchGiphy(obj) {
+      const q = obj.nameImg;
+
+      const url = 'https://api.giphy.com/v1/gifs/search?api_key=yvwA8IzIyat8LWFNiIbVz6BqNGAz6n8N&q='+q+'&limit=5&offset=0&rating=G&lang=en';
+
+      Axios.get(url).then(
+        (res) =>{
+          console.log('res');
+          console.log(res.data.data);
+          console.log('this.images');
+          this.images = res.data.data;
+          console.log(this.images);
+      })
+      .catch(error => {
+          console.log(error.response)
+      });
+
+    }
   }
 }
 </script>
@@ -23,6 +68,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+img.vueLogo{ max-width: 70px; }
+ul li{
+  list-style: none;
+  display: inline-block;
+}
+
+ul li img{
+  width: auto;
+  max-width: 100px;
+  margin-right: 10px;
 }
 </style>
---
+++ giphy-search/src/components/GiphyForm.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="form">
+        <h1>Giphy Search</h1>
+        <form @submit.prevent="onSubmit">
+            <input 
+                type="text"
+                v-model="nameImg"
+            >
+
+            <button
+
+            >
+                Buscar
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GiphyForm',
+        data()  {
+            return {
+                nameImg: ''
+            }
+        },
+        methods: {
+            onSubmit() {
+                console.log('submited');
+                this.$emit('searchGiphy', {
+                    nameImg: this.nameImg
+                });
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    .form{
+        background-color:#f5f5f5;
+        border-radius: 10px;
+        max-width: 500px;
+        margin: 20px auto;
+        padding: 20px;
+    }
+
+    .form input{
+        display: block;
+        width: 200px;
+        height: 30px;
+        margin: auto;
+        margin-bottom: 10px;
+        border: 1px solid #80bdff;
+        border-radius: 5px;
+        padding: 5px 10px;
+        outline: 0;
+        box-shadow: 0 0 0 1px rgba(0,123,255,.25);
+    }
+
+    .form button{
+        background-color: dodgerblue;
+        color: #fff;
+        padding: 10px;
+        width: 220px;
+        border: none;
+        border-radius: 5px;
+        margin-top: 10px;
+    }
+
+    .form h1{
+        margin: 0;
+    }
+
+    .form button:hover{
+        cursor: pointer;
+        opacity: .9;
+    }
+
+</style>
